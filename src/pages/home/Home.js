4d85387d968636550e529011384aef9f14ba2233
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-12-22 12:49:07 -0700

    Create RecipeList and add to Home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,16 @@
 import "./Home.css";
 
+import RecipeList from "../../components/RecipeList";
+import { useFetch } from "../../hooks/useFetch";
+
 export default function Home() {
-  return <div></div>;
+  const { data: recipes, isPending, error } = useFetch("http://localhost:3001/recipes");
+
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipes && <RecipeList recipes={recipes} />}
+    </div>
+  );
 }
