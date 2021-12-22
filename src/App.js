719f20634f4f688898a385e877f860a5f86e264f
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-12-22 11:04:25 -0700

    Component templates and Routes setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Create from "./pages/create/Create";
+import Home from "./pages/home/Home";
+import Recipe from "./pages/recipe/Recipe";
+import Search from "./pages/search/Search";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="create" element={<Create />} />
+          <Route path="search" element={<Search />} />
+          <Route path="recipes/:id" element={<Recipe />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
