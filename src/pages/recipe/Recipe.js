b07b628ffb77f6184d4483f19346474f591be7d4
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-12-22 13:10:57 -0700

    Fetch and display info on Recipe page

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,5 +1,27 @@
 import "./Recipe.css";
 
+import { useFetch } from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
+
 export default function Recipe() {
-  return <div></div>;
+  const { id } = useParams();
+  const { data: recipe, isPending, error } = useFetch(`http://localhost:3001/recipes/${id}`);
+  return (
+    <div className="recipe">
+      {error && <div className="error">{error}</div>}
+      {isPending && <div className="loading">Loading...</div>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>{recipe.cookingTime} to cook</p>
+          <ul>
+            {recipe.ingredients.map((ingredient) => (
+              <li key={ingredient}>{ingredient}</li>
+            ))}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
+    </div>
+  );
 }
