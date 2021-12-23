64d17e2e8a9f94a417916a24d893e2745b976ecc
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-12-23 15:22:25 -0700

    Define dark/light mode toggle

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -1,14 +1,27 @@
 import "./ThemeSelector.css";
 
+import modeIcon from "../assets/mode-icon.svg";
 import { useTheme } from "../hooks/useTheme";
 
 const themeColors = ["#063970", "#700639", "#397006"];
 
 export default function ThemeSelector() {
-  const { changeColor } = useTheme();
+  const { changeColor, changeMode, mode } = useTheme();
+
+  const toggleMode = () => {
+    changeMode(mode === "dark" ? "light" : "dark");
+  };
 
   return (
     <div className="theme-selector">
+      <div className="mode-toggle">
+        <img
+          src={modeIcon}
+          onClick={toggleMode}
+          style={{ filter: mode === "dark" ? "invert(100%)" : "invert(20%)" }}
+          alt="Mode Toggle"
+        />
+      </div>
       <div className="theme-buttons">
         {themeColors.map((color) => (
           <div key={color} onClick={() => changeColor(color)} style={{ background: color }} />
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -6,19 +6,27 @@ const themeReducer = (state, action) => {
   switch (action.type) {
     case "CHANGE_COLOR":
       return { ...state, color: action.payload };
+    case "CHANGE_MODE":
+      return { ...state, mode: action.payload };
     default:
       return state;
   }
 };
 
 export function ThemeProvider({ children }) {
-  const [state, dispatch] = useReducer(themeReducer, { color: "#063970" });
+  const [state, dispatch] = useReducer(themeReducer, { color: "#063970", mode: "dark" });
 
   const changeColor = (color) => {
     dispatch({ type: "CHANGE_COLOR", payload: color });
   };
 
+  const changeMode = (mode) => {
+    dispatch({ type: "CHANGE_MODE", payload: mode });
+  };
+
   return (
-    <ThemeContext.Provider value={{ ...state, changeColor }}>{children}</ThemeContext.Provider>
+    <ThemeContext.Provider value={{ ...state, changeColor, changeMode }}>
+      {children}
+    </ThemeContext.Provider>
   );
 }
