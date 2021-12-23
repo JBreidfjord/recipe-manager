f6c258e052ef14d9b4978b77718a243a2b8cebc0
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-12-23 13:57:09 -0700

    Implement search results page

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,5 +1,21 @@
-import "./Search.css";
+import RecipeList from "../../components/RecipeList";
+import { useFetch } from "../../hooks/useFetch";
+import { useLocation } from "react-router-dom";
 
 export default function Search() {
-  return <div></div>;
+  // const { q: query } = new URLSearchParams(useLocation());
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get("q");
+  const url = "http://localhost:3001/recipes?q=" + query;
+  const { data: recipes, isPending, error } = useFetch(url);
+
+  return (
+    <div>
+      <h2 className="page-title">Recipes including "{query}"</h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipes && <RecipeList recipes={recipes} />}
+    </div>
+  );
 }
